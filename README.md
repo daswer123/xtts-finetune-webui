39611b72e2e464ad5d99dd1b8693bf236bf09d04
Danil Boldyrev <daswerq123@gmail.com>
2023-12-04 15:06:20 +0300

    Added a couple of handy things

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # xtts-finetune-webui
+
 This webui is a slightly modified copy of the [official webui](https://github.com/coqui-ai/TTS/pull/3296) for finetune xtts.
 
 ## Key features:
-1) Updated faster-whisper to 0.10.0 with the ability to select a larger-v3 model.
-2) Added the ability to select the base model for XTTS, as well as when you re-training does not need to download the model again.
-3) Added ability to select custom model as base model during training, which will allow finetune already finetune model.
-4) Added possibility to get optimized version of the model for 1 click ( step 2.5, put optimized version in output folder).
-5) Changed output folder to output folder inside the main folder.
-6) Added possibility to customize infer settings during model checking.
+
+1. Updated faster-whisper to 0.10.0 with the ability to select a larger-v3 model.
+2. Added the ability to select the base model for XTTS, as well as when you re-training does not need to download the model again.
+3. Added ability to select custom model as base model during training, which will allow finetune already finetune model.
+4. Added possibility to get optimized version of the model for 1 click ( step 2.5, put optimized version in output folder).
+5. Changed output folder to output folder inside the main folder.
+6. Added possibility to customize infer settings during model checking.
+7. If you accidentally restart the interface during one of the steps, you can load data to additional buttons
+8. You can choose whether to delete training folders after you have optimized the model
 
 ## Changes in webui
+
 ### 1 - Data processing
 
 ![image](https://github.com/daswer123/xtts-finetune-webui/assets/22278673/8f09b829-098b-48f5-9668-832e7319403b)
@@ -22,15 +27,16 @@ This webui is a slightly modified copy of the [official webui](https://github.co
 
 ![image](https://github.com/daswer123/xtts-finetune-webui/assets/22278673/dade10d3-82f3-4458-a647-c82081ad1ff8)
 
-
 ## Install
-1) Make sure you have `Cuda` installed
-2) `git clone https://github.com/daswer123/xtts-finetune-webui`
-3) `cd xtts-finetune-webui`
-4) `pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118`
-5) `pip install -r requirements.txt`
+
+1. Make sure you have `Cuda` installed
+2. `git clone https://github.com/daswer123/xtts-finetune-webui`
+3. `cd xtts-finetune-webui`
+4. `pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118`
+5. `pip install -r requirements.txt`
 
 ### If you're using Windows
-1) First start `install.bat`
-2) To start the server start `start.bat`
-3) Go to the local address `127.0.0.1:5003`
+
+1. First start `install.bat`
+2. To start the server start `start.bat`
+3. Go to the local address `127.0.0.1:5003`
--- xtts_demo.py
+++ xtts_demo.py
@@ -6,6 +6,7 @@ from pathlib import Path
 
 import os
 import shutil
+import glob
 
 import gradio as gr
 import librosa.display
@@ -97,6 +98,37 @@ def run_tts(lang, tts_text, speaker_audio_file, temperature, length_penalty,repe
     return "Speech generated !", out_path, speaker_audio_file
 
 
+def load_params_tts(out_path,version):
+    
+    out_path = Path(out_path)
+
+    base_model_path = Path.cwd() / "base_models" / version 
+
+    if not base_model_path.exists():
+        return "Base model not found !","","",""
+
+    vocab_path = base_model_path / "vocab.json"
+    config_path = base_model_path / "config.json"
+
+    model_path = out_path / "model.pth"
+    run_path = out_path / "run" / "training"
+
+    # Using glob to find all directories starting with 'GPT_XTTS_FT'
+    gpt_dirs = list(run_path.glob('GPT_XTTS_FT*'))
+    # print(model_path.exists(),run_path,gpt_dirs)
+
+    if not model_path.exists():
+       # Using glob to find all directories starting with 'GPT_XTTS_FT'
+        gpt_dirs = list(run_path.glob('GPT_XTTS_FT*'))
+
+        if gpt_dirs:
+            model_path = find_latest_best_model(gpt_dirs[0])
+        else:
+            return "Params for TTS not found", "", "", ""
+    
+
+    return "Params for TTS loaded", model_path, config_path, vocab_path
+     
 
 
 # define a logger to redirect 
@@ -132,10 +164,11 @@ logging.basicConfig(
     ]
 )
 
-def read_logs():
-    sys.stdout.flush()
-    with open(sys.stdout.log_file, "r") as f:
-        return f.read()
+# def read_logs():
+#     return ""
+    # sys.stdout.flush()
+    # with open(sys.stdout.log_file, "r") as f:
+    #     return f.read()
 
 
 if __name__ == "__main__":
@@ -212,6 +245,7 @@ if __name__ == "__main__":
                     "large-v2",
                     "large",
                     "medium",
+                    "small"
                 ],
             )
 
@@ -244,7 +278,7 @@ if __name__ == "__main__":
                 label="Logs:",
                 interactive=False,
             )
-            demo.load(read_logs, None, logs, every=1)
+            # demo.load(read_logs, None, logs, every=1)
 
             prompt_compute_btn = gr.Button(value="Step 1 - Create dataset")
         
@@ -262,7 +296,7 @@ if __name__ == "__main__":
                         error = traceback.format_exc()
                         return f"The data processing was interrupted due an error !! Please check the console to verify the full error message! \n Error summary: {error}", "", ""
 
-                clear_gpu_cache()
+                # clear_gpu_cache()
 
                 # if audio total len is less than 2 minutes raise an error
                 if audio_total_size < 120:
@@ -274,6 +308,7 @@ if __name__ == "__main__":
                 return "Dataset Processed!", train_meta, eval_meta
 
         with gr.Tab("2 - Fine-tuning XTTS Encoder"):
+            load_params_btn = gr.Button(value="Load Params from output folder")
             version = gr.Dropdown(
                 label="XTTS base version",
                 value="v2.0.2",
@@ -323,16 +358,36 @@ if __name__ == "__main__":
                 step=1,
                 value=args.max_audio_length,
             )
+            max_audio_length = gr.Slider(
+                label="Max permitted audio size in seconds:",
+                minimum=2,
+                maximum=20,
+                step=1,
+                value=args.max_audio_length,
+            )
+
+            clear_train_data = gr.Dropdown(
+                label="Clear train data, you will delete selected folder, after optimizing",
+                value="none",
+                choices=[
+                    "none",
+                    "run",
+                    "dataset",
+                    "all"
+                ])
+            
             progress_train = gr.Label(
                 label="Progress:"
             )
+
+            # demo.load(read_logs, None, logs_tts_train, every=1)
+            train_btn = gr.Button(value="Step 2 - Run the training")
+            optimize_model_btn = gr.Button(value="Step 2.5 - Optimize the model")
+            
             logs_tts_train = gr.Textbox(
                 label="Logs:",
                 interactive=False,
             )
-            demo.load(read_logs, None, logs_tts_train, every=1)
-            train_btn = gr.Button(value="Step 2 - Run the training")
-            optimize_model_btn = gr.Button(value="Step 2.5 - Optimize the model")
 
             def train_model(custom_model,version,language, train_csv, eval_csv, num_epochs, batch_size, grad_acumm, output_path, max_audio_length):
                 clear_gpu_cache()
@@ -356,7 +411,7 @@ if __name__ == "__main__":
                 clear_gpu_cache()
                 return "Model training done!", config_path, vocab_file, ft_xtts_checkpoint, speaker_wav
 
-            def optimize_model(xtts_checkpoint,out_path):
+            def optimize_model(xtts_checkpoint,out_path,clear_train_data):
                 clear_gpu_cache()
 
                 checkpoint_dir = os.path.dirname(xtts_checkpoint)
@@ -379,20 +434,37 @@ if __name__ == "__main__":
                 dataset_dir = out_path / "dataset"
 
                 if run_dir.exists() and dataset_dir.exists():
-                    # shutil.rmtree(run_dir)
-                    # shutil.rmtree(dataset_dir)
+                    if clear_train_data == "run" or clear_train_data == "all":
+                      shutil.rmtree(run_dir)
+                    if clear_train_data == "dataset" or clear_train_data == "all":
+                      shutil.rmtree(dataset_dir)
 
-                 clear_gpu_cache()
+                clear_gpu_cache()
 
-                 model_path = out_path / "model.pth"
+                model_path = out_path / "model.pth"
+                ft_xtts_checkpoint = str(model_path)
  
-                 ft_xtts_checkpoint = str(model_path)
- 
-                 return "Model optimized!",ft_xtts_checkpoint
+                return "Model optimized!",ft_xtts_checkpoint
+
+            def load_params(out_path):
+                path_output = Path(out_path)
+                
+                dataset_path = path_output / "dataset"
+
+                if not dataset_path.exists():
+                    return "The output folder does not exist!", "", ""
+
+                eval_train = dataset_path / "metadata_train.csv"
+                eval_csv = dataset_path / "metadata_eval.csv"
+
+                clear_gpu_cache()
+            
+                return "The data has been updated", eval_train, eval_csv
 
         with gr.Tab("3 - Inference"):
             with gr.Row():
                 with gr.Column() as col1:
+                    load_params_tts_btn = gr.Button(value="Load params for TTS from output folder")
                     xtts_checkpoint = gr.Textbox(
                         label="XTTS checkpoint path:",
                         value="",
@@ -510,6 +582,16 @@ if __name__ == "__main__":
                 ],
             )
 
+            load_params_btn.click(
+                fn=load_params,
+                inputs=[out_path],
+                outputs=[
+                    progress_train,
+                    train_csv,
+                    eval_csv,
+                ]
+            )
+
 
             train_btn.click(
                 fn=train_model,
@@ -532,7 +614,8 @@ if __name__ == "__main__":
                 fn=optimize_model,
                 inputs=[
                     xtts_checkpoint,
-                    out_path
+                    out_path,
+                    clear_train_data
                 ],
                 outputs=[progress_train,xtts_checkpoint],
             )
@@ -562,6 +645,15 @@ if __name__ == "__main__":
                     use_config
                 ],
                 outputs=[progress_gen, tts_output_audio, reference_audio],
+            )
+
+            load_params_tts_btn.click(
+                fn=load_params_tts,
+                inputs=[
+                    out_path,
+                    version
+                    ],
+                outputs=[progress_load,xtts_checkpoint,xtts_config,xtts_vocab],
             )
 
     demo.launch(
